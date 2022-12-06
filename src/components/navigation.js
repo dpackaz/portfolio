1221244e55363d6d2e1efe0e3e74ba768839bbbe
Daniel Packer <danielapacker@gmail.com>
2022-12-05 22:38:52 -0700

    add navigation.js

---
+++ src/components/navigation.js
@@ -0,0 +1,46 @@
+import React from "react";
+
+function Navigation({ currentPage, handlePageChange }) {
+  return (
+    <ul className="nav nav-tabs">
+      <li className="nav-item">
+        <a
+          href="#home"
+          onClick={() => handlePageChange("Home")}
+          className={currentPage === "Home" ? "nav-link active" : "nav-link"}
+        >
+          Home
+        </a>
+      </li>
+      <li className="nav-item">
+        <a
+          href="#projects"
+          onClick={() => handlePageChange("Projects")}
+          className={currentPage === "About" ? "nav-link active" : "nav-link"}
+        >
+          Projects
+        </a>
+      </li>
+      <li className="nav-item">
+        <a
+          href="#resume"
+          onClick={() => handlePageChange("Resume")}
+          className={currentPage === "Blog" ? "nav-link active" : "nav-link"}
+        >
+          Resume
+        </a>
+      </li>
+      <li className="nav-item">
+        <a
+          href="#contact"
+          onClick={() => handlePageChange("Contact")}
+          className={currentPage === "Contact" ? "nav-link active" : "nav-link"}
+        >
+          Contact
+        </a>
+      </li>
+    </ul>
+  );
+}
+
+export default Navigation;
