fdff3da17f84989663f3c355a3e6275322bb413a
Daniel Packer <danielapacker@gmail.com>
2022-12-06 00:13:07 -0700

    add some minor changes to home and resume.js to attempt first deployment

--- src/components/pages/home.js
+++ src/components/pages/home.js
@@ -1,22 +1,22 @@
 import React from "react";
 
-export default function Home() {
+function Home() {
   return (
-    <div>
-      <h1 className="accentText">Hi, I'm Daniel!</h1>
-      <div className="mainBackground">
+    <div className="jumbotron jumbotron-fluid">
+      <div className="container mainBackground">
+        <h1 className="display-4 greeting">Hi, I'm Daniel!</h1>
         <img
           className="rounded mx-auto d-block"
           alt="Daniel Packer"
           src="./public/daniel-packer.jpg"
         ></img>
-        <p className="basicText">
+        <p className="lead basicText">
           My name is Daniel Packer, and I am a full-stack web developer and
           digital marketing professional. I can build the site you want and then
           bring people to it.
         </p>
         <br></br>
-        <p className="basicText">
+        <p className="lead basicText">
           I love to make things and see how people react to them, so outside of
           my career I love things like cooking, wood working, and design. I am
           an Arizona native, and I currently live in Gilbert, AZ with my wife,
@@ -26,3 +26,5 @@ export default function Home() {
     </div>
   );
 }
+
+export default Home;
